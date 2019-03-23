939b59896f7b418c179ff7c78c16992d50079de1
ikar <bolegvv@gmail.com>
2019-03-23 19:55:39 +0200

    [Fix] Fixed identation

--- CryptoGraphica/Defaults.swift
+++ CryptoGraphica/Defaults.swift
@@ -9,24 +9,24 @@
 import Foundation
 
 struct Defaults {
-  static let initalShift = 7
-  static let bytesPerPixel = 4
-  static let bitsPerComponent = 8
-  static let bytesOfLength = 4
-  
-  static let dataPrefix = "<m>"
-  static let dataSuffix = "</m>"
-  
-  static var sizeOfInfoLength: Int {
-    return bytesOfLength * bitsPerComponent
-  }
-  
-  static var minPixelsToMessage: Int {
-    return (dataPrefix.count + dataSuffix.count) * bitsPerComponent
-  }
-  
-  static var minPixels: Int {
-    return sizeOfInfoLength + minPixelsToMessage
-  }
-  
+    static let initalShift = 7
+    static let bytesPerPixel = 4
+    static let bitsPerComponent = 8
+    static let bytesOfLength = 4
+    
+    static let dataPrefix = "<m>"
+    static let dataSuffix = "</m>"
+    
+    static var sizeOfInfoLength: Int {
+        return bytesOfLength * bitsPerComponent
+    }
+    
+    static var minPixelsToMessage: Int {
+        return (dataPrefix.count + dataSuffix.count) * bitsPerComponent
+    }
+    
+    static var minPixels: Int {
+        return sizeOfInfoLength + minPixelsToMessage
+    }
+    
 }
--- CryptoGraphica/Utilities.swift
+++ CryptoGraphica/Utilities.swift
@@ -9,36 +9,36 @@
 import Foundation
 
 struct Utilities {
-  static func Mask8(_ x: UInt32) -> UInt32{
-    return x & 0xFF
-  }
-  
-  static func Color(_ x: UInt32, shift: Int) -> UInt32 {
-    return Mask8(x >> (8 * UInt32(shift)))
-  }
-  
-  static func AddBits(number1: UInt32, number2: UInt32, shift: Int) -> UInt32 {
-    return (number1 | Mask8(number2) << (8 * UInt32(shift)))
-  }
-  
-  static func NewPixel(pixel: UInt32, shiftedBits: UInt32, shift: Int) -> UInt32 {
-    let bit = (shiftedBits & 1) << (8 * UInt32(shift))
-    let colorAndNot = (pixel & ~(1 << (8 * UInt32(shift))))
-    return colorAndNot | bit
-  }
-  
-  static func ColorToStep(step: UInt32) -> PixelColor {
-    if step % 3 == 0 {
-      return .blue;
-    } else if step % 2 == 0 {
-      return .green;
-    } else {
-      return .red;
+    static func Mask8(_ x: UInt32) -> UInt32{
+        return x & 0xFF
     }
-  }
+    
+    static func Color(_ x: UInt32, shift: Int) -> UInt32 {
+        return Mask8(x >> (8 * UInt32(shift)))
+    }
+    
+    static func AddBits(number1: UInt32, number2: UInt32, shift: Int) -> UInt32 {
+        return (number1 | Mask8(number2) << (8 * UInt32(shift)))
+    }
+    
+    static func NewPixel(pixel: UInt32, shiftedBits: UInt32, shift: Int) -> UInt32 {
+        let bit = (shiftedBits & 1) << (8 * UInt32(shift))
+        let colorAndNot = (pixel & ~(1 << (8 * UInt32(shift))))
+        return colorAndNot | bit
+    }
+    
+    static func ColorToStep(step: UInt32) -> PixelColor {
+        if step % 3 == 0 {
+            return .blue;
+        } else if step % 2 == 0 {
+            return .green;
+        } else {
+            return .red;
+        }
+    }
 }
 
 
 enum PixelColor: Int {
-  case red = 0, green, blue
+    case red = 0, green, blue
 }
